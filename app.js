6be28d3cd7a86937f8aada5f6bc6ced1faa5326c
Alli Pao <allipao@Allis-MacBook-Pro.local>
2019-03-21 19:12:22 -0400

    Added css

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
